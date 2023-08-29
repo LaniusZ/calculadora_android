7156c839f2ffffb2a42dd3170befb0e8fa5c9260
unknown <SOPORTE TI@TEAPLABBPROF-22.labtemuco.aiep.corp>
2023-08-29 19:57:33 -0400

    comit inicial de calculadora

---
+++ app/src/main/java/alamos/alfonso/calculadora/MainActivity.kt
@@ -0,0 +1,59 @@
+package calcu2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    var total : Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun pressBtn1 (view: View) {
+        var txt2: TextView = findViewById(R.id.txt2)
+
+        txt2.text = txt2.text.toString() + "1"
+    }
+
+    fun pressBtn2 (view: View) {
+        var txt2: TextView = findViewById(R.id.txt2)
+
+        txt2.text = txt2.text.toString() + "2"
+    }
+
+    fun pressBtnMas (view: View) {
+        var txt1: TextView = findViewById(R.id.txt1)
+        var txt2: TextView = findViewById(R.id.txt2)
+
+        total += txt2.text.toString().toInt()
+
+        txt1.text = txt1.text.toString() + txt2.text.toString() + "+"
+        txt2.text = ""
+    }
+
+    fun pressBtnEq (view: View) {
+        var txt1: TextView = findViewById(R.id.txt1)
+        var txt2: TextView = findViewById(R.id.txt2)
+
+        total += txt2.text.toString().toInt()
+
+        txt1.text = txt1.text.toString() + txt2.text.toString() + "="
+        txt2.text = total.toString()
+    }
+
+    fun pressBtnC (view: View) {
+        var txt1: TextView = findViewById(R.id.txt1)
+        var txt2: TextView = findViewById(R.id.txt2)
+
+        total = 0
+
+        txt1.text = ""
+        txt2.text = ""
+    }
+
+}
